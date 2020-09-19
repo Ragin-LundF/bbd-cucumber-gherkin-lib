0e2d2c73ed0c1af911dc5928e50dfb8afc50fc42
Ragin666 <ragin666@gmail.com>
2020-09-19 02:56:16 +0200

    get/post tests

--- src/test/java/com/ragin/bdd/cucumbertests/library/test/Authentication.java
+++ src/test/java/com/ragin/bdd/cucumbertests/library/test/Authentication.java
@@ -6,7 +6,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,14 +19,62 @@ public class Authentication {
     @Value("${cucumberTest.authorization.bearerToken.default}")
     private String token;
 
-    @GetMapping("/api/v1/unauthenticated")
-    public ResponseEntity<String> stubUnauthenticated() {
+    @GetMapping("/api/v1/unauthorized")
+    public ResponseEntity<String> stubUnauthorizedGet() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(createUnauthorizedResponse());
+    }
+
+    @DeleteMapping("/api/v1/unauthorized")
+    public ResponseEntity<String> stubUnauthorizedDelete(final @RequestBody String body) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(createUnauthorizedResponse());
+    }
+
+    @PostMapping("/api/v1/unauthorized")
+    public ResponseEntity<String> stubUnauthorizedPost(final @RequestBody String body) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(createUnauthorizedResponse());
+    }
+
+    @PutMapping("/api/v1/unauthorized")
+    public ResponseEntity<String> stubUnauthorizedPut(final @RequestBody String body) {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(createUnauthorizedResponse());
     }
 
     @GetMapping("/api/v1/authorized")
-    public ResponseEntity<String> stubAuthenticatedWithToken(
+    public ResponseEntity<String> stubAuthenticatedWithTokenGet(
             final @NonNull @RequestHeader("Authorization") String authToken
+    ) {
+        if (authToken.equals("Bearer " + token)) {
+            return ResponseEntity.ok().body(createAuthorizedResponse());
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(createUnauthorizedResponse());
+    }
+
+    @DeleteMapping("/api/v1/authorized")
+    public ResponseEntity<String> stubAuthenticatedWithTokenDelete(
+            final @NonNull @RequestHeader("Authorization") String authToken,
+            final @RequestBody String body
+    ) {
+        if (authToken.equals("Bearer " + token)) {
+            return ResponseEntity.ok().body(createAuthorizedResponse());
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(createUnauthorizedResponse());
+    }
+
+    @PostMapping("/api/v1/authorized")
+    public ResponseEntity<String> stubAuthenticatedWithTokenPost(
+            final @NonNull @RequestHeader("Authorization") String authToken,
+            final @RequestBody String body
+    ) {
+        if (authToken.equals("Bearer " + token)) {
+            return ResponseEntity.ok().body(createAuthorizedResponse());
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(createUnauthorizedResponse());
+    }
+
+    @PutMapping("/api/v1/authorized")
+    public ResponseEntity<String> stubAuthenticatedWithTokenPut(
+            final @NonNull @RequestHeader("Authorization") String authToken,
+            final @RequestBody String body
     ) {
         if (authToken.equals("Bearer " + token)) {
             return ResponseEntity.ok().body(createAuthorizedResponse());
